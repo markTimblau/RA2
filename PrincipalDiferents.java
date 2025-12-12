2647865461be54557f3adab3a25f0d86e4dddd43
markTimblau <2024_mark.timblau@iticbcn.cat>
2025-12-12 16:27:56 +0100

    Terminado el segundo comportamiento

--- PrincipalDiferents.java
+++ PrincipalDiferents.java
@@ -1,10 +1,10 @@
-public class PrincipalIguals {
+public class PrincipalDiferents {
     public static void main(String[] args) {
         //CREAMOS LOS OBJETOS
         Fil fil1 = new Fil("Juan");
         Fil fil2 = new Fil("Pepe");
         //LES DAMOS PRIORIDADES
-        fil1.setPriority(Thread.MAX_PRIORITY);
+        fil1.setPriority(Thread.MIN_PRIORITY);
         fil2.setPriority(Thread.MAX_PRIORITY);
         //LOS LLAMAMOS
         fil1.start();
