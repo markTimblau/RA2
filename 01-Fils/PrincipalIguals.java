bcb5da39979aa9a9a8739c4c03e6edbd1f8d228d
markTimblau <2024_mark.timblau@iticbcn.cat>
2025-12-12 16:26:15 +0100

    Añadido las prioridades correctas y comentarios

--- 01-Fils/PrincipalIguals.java
+++ 01-Fils/PrincipalIguals.java
@@ -2,6 +2,8 @@ public class PrincipalIguals {
     public static void main(String[] args) {
         Fil fil1 = new Fil("Juan");
         Fil fil2 = new Fil("Pepe");
+        fil1.setPriority(Thread.MAX_PRIORITY);
+        fil2.setPriority(Thread.MAX_PRIORITY);
         fil1.start();
         fil2.start();
         System.out.println("Acaba thread main");
